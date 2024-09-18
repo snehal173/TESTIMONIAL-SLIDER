040380edd696b119576d52e4fd892df3f6d70c3a
Your Name <you@example.com>
2024-09-18 19:31:20 +0530

    First Commit

---
+++ testimonal_page/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import React from 'react'
+import reviews from "./Data";
+import Testimonial from './components/Testimonial';
+
+ const App = () => {
+  return (
+    <div className='flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200'>
+      <div className='text-center'>
+      <h1 className='text-4xl font-bold'>Our Testimonials</h1> 
+     
+      <div className='bg-violet-400 h-[4px] max-w-[150px] mt-1 mx-auto'> </div>
+      <Testimonial reviews={reviews}/>
+       </div >
+    </div>
+  )
+}
+
+
+export default App
